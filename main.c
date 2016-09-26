bb0628307e8c6fe3cf4e9c3902fe275d018b1da0
tankers746 <tankers746@live.com>
2016-09-26 08:52:21 +0800

    Initial commit

---
+++ main.c
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: Tom
+ *
+ * Created on 26 September 2016, 8:42 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("test");
+    fprintf(stderr, "test");
+    return (EXIT_SUCCESS);
+}
+
