e24278f55076c918d6e21501e537657e7130614f
wrsx <mewithmyclub@hotmail.com>
2016-09-26 16:12:07 +0800

    Work hard play hard

--- ColumnMatrixCollisions/ColumnMatrixCollisions.c
+++ ColumnMatrixCollisions/ColumnMatrixCollisions.c
@@ -1,31 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   main.c
- * Author: Tom
- *
- * Created on 26 September 2016, 8:42 AM
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 /*
  * 
  */
-int main(int argc, char** argv) {
+
+float mat[20][10]; //test dimensions
+long keys[20];
+
+void loadMatrix() {
+	
 	char buffer[1024];
 	char *record, *line;
 	int i = 0, j = 0;
-	int mat[4400][500];
-	FILE *fstream = fopen("\data.txt", "r");
+	FILE *fstream = fopen("testdata.txt", "r");
 	if (fstream == NULL)
 	{
-		printf("\n file opening failed ");
+		printf("\n Opening matrix failed ");
 		return -1;
 	}
 	while ((line = fgets(buffer, sizeof(buffer), fstream)) != NULL)
@@ -33,18 +24,54 @@ int main(int argc, char** argv) {
 		record = strtok(line, ",");
 		while (record != NULL)
 		{
-			printf("record : %s", record);    //here you can put the record into the array as per your requirement.
-			mat[i][j++] = atoi(record);
+			//printf("Recording: to [%d][%d] \n", i, j);
+			mat[i][j++] = atof(record);
+			record = strtok(NULL, ",");
+		}
+		j = 0;
+		++i;
+	}
+}
+
+void loadKeys() {
+	/*char buffer[1024];
+	char *record, *line;
+	int i = 0;
+	FILE *fstream = fopen("testkeys.txt", "r");
+	if (fstream == NULL)
+	{
+		printf("\n Opening key vector failed ");
+		return -1;
+	}
+	while ((line = fgets(buffer, sizeof(buffer), fstream)) != NULL)
+	{
+		record = strtok(line, " ");
+		while (record != NULL)
+		{
+			printf("Recording: %l to [%d] \n", record, i);
+			keys[i] = atol(record);
 			record = strtok(NULL, ",");
 		}
 		++i;
 	}
-	for (int k = 0; k < 100; k++) {
-		printf(mat[k][0]);
+	*/
+}
+
+int main(int argc, char** argv) {
+	loadMatrix();
+	loadKeys();
+	for (int i = 0; i < 20; i++) {
+		for (int j = 0; j < 10; j++) {
+			printf("%f ", mat[i][j]);
+		}
+		printf("\n");
 	}
 
-    printf("test");
-    fprintf(stderr, "test");
+	/*for (int i = 0; i < 20; i++) {
+		printf("%d \n", keys[i]);
+	}
+	*/
+	
     return (EXIT_SUCCESS);
 }
 
