74ceac2da5eb922a0caab87d8ad491094cd161b7
tankers746 <tankers746@live.com>
2016-09-26 11:31:18 +0800

    Fixed depreciation errors

--- ColumnMatrixCollisions/ColumnMatrixCollisions.c
+++ ColumnMatrixCollisions/ColumnMatrixCollisions.c
@@ -13,11 +13,36 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 /*
  * 
  */
 int main(int argc, char** argv) {
+	char buffer[1024];
+	char *record, *line;
+	int i = 0, j = 0;
+	int mat[4400][500];
+	FILE *fstream = fopen("\data.txt", "r");
+	if (fstream == NULL)
+	{
+		printf("\n file opening failed ");
+		return -1;
+	}
+	while ((line = fgets(buffer, sizeof(buffer), fstream)) != NULL)
+	{
+		record = strtok(line, ",");
+		while (record != NULL)
+		{
+			printf("record : %s", record);    //here you can put the record into the array as per your requirement.
+			mat[i][j++] = atoi(record);
+			record = strtok(NULL, ",");
+		}
+		++i;
+	}
+	for (int k = 0; k < 100; k++) {
+		printf(mat[k][0]);
+	}
+
     printf("test");
     fprintf(stderr, "test");
     return (EXIT_SUCCESS);
