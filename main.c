cab0e5e6e6a684d3b374b4603aa0eedc4e24842e
Tom <tankers746@live.com>
2016-10-01 01:01:00 +0800

    Added block finding

--- main.c
+++ main.c
@@ -2,11 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <math.h>
 /*
  * 
  */
 
-float mat[20][10]; //test dimensions
+float mat[10][20]; //test dimensions
 char keys[20][15];
 
 int loadMatrix() {
@@ -26,7 +27,7 @@ int loadMatrix() {
         while (record != NULL)
         {
             //printf("Recording: to [%d][%d] \n", i, j);
-            mat[i][j++] = atof(record);
+            mat[j++][i] = atof(record);
             record = strtok(NULL, ",");
         }
         j = 0;
@@ -61,8 +62,7 @@ int loadKeys() {
 }
 
 char* genBlock(int col[], int colSize) {
-    int dia = 0.000001;
-    int 
+    double dia = 0.000001; 
     bool within_dia[colSize][colSize];
     for (int i = 0; i < colSize; i++) {
         for (int j = 1; j < colSize; j++) {
@@ -77,17 +77,82 @@ char* genBlock(int col[], int colSize) {
     return 0;
 }
 
+int floatcomp(const void* elem1, const void* elem2) {
+    if(*(const float*)elem1 < *(const float*)elem2) {
+    	return -1;
+    }
+    return *(const float*)elem1 > *(const float*)elem2;
+}
+ 
+void findBlocks(float col[], int colSize, float dia) { 	
+    qsort(col, colSize, sizeof(float), floatcomp);
+
+    float min, max = 0;
+    float result[colSize][colSize];
+    float temp[colSize];    
+    memset(result, 0, sizeof(result[0][0]) * colSize * colSize);
+    memset(temp, 0, sizeof(temp));
+    int tempCount = 0;
+    int resultCount = 0;
+
+    for(int i = 0; i < colSize; i++) {
+        //fprintf(stderr,"[%d] %f\n",i, col[i]);
+
+        if(temp[0] == 0) {
+            min = max = col[i];
+            temp[tempCount++] = col[i];    
+        } else {
+            if (fabs(col[i] - min) < dia && fabs(col[i] - max) < dia && tempCount < 4) {
+                temp[tempCount++] = col[i];
+                if(col[i] < min) {
+                    min = col[i];
+                } else if(col[i] > max) {
+                    max = col[i];
+                }
+
+            } else {
+                if(temp[0] != 0 && tempCount > 3) {
+                     for(int j = 0; j < tempCount; j++) {
+                        result[resultCount][j] = temp[j];
+                    }
+                    resultCount++;
+                }
+                min = max = col[i];
+                memset(temp, 0, sizeof(temp));
+                temp[0] = col[i];
+                i = i - tempCount;
+                tempCount = 0;
+            }
+        }      
+    }
+
+    for(int j = 0; j < resultCount; j++) {
+        int k = 0;
+        printf("[");
+        while(result[j][k] != 0) {
+                printf("%f, ", result[j][k++]);
+        }
+        printf("]\n");
+
+    }
+     
+}
+ 
 int main(int argc, char* argv[]) {
     loadMatrix();
     loadKeys();
-    
+    findBlocks(mat[0], 20, 0.01);
+
     /*
+    
     for (int i = 0; i < 20; i++) {
         for (int j = 0; j < 10; j++) {
-            printf("%f ", mat[i][j]);
+            printf("%f ", mat[j][i]);
         }
         printf("\n");
     }
+    */
+    /*
     for (int i = 0; i < 20; i++) {
         printf("%s \n", keys[i]);
     }*/
