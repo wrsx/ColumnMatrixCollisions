3ea81ebd4737beb871a24a5bce0f0c5ea565ed6c
wrsx <mewithmyclub@hotmail.com>
2016-10-05 03:22:10 +0800

    Key addition function

--- openmp.c
+++ openmp.c
@@ -72,7 +72,7 @@ int main ( int argc, char *argv[] ) {
     }
     */
 
-    //Reverse the key for easier addition
+    //String reverser for easier addition
     void reverseKey(char *key) {
         char temp;
         int i = 0;
@@ -85,34 +85,64 @@ int main ( int argc, char *argv[] ) {
             j--;
         }
     }
-    
-    int loadKeys() {
-        char buffer[1024];
-        char *record, *line;
-        int i = 0;
-        FILE *fstream = fopen("testkeys.txt", "r");
-        if (fstream == NULL) {
-            printf("\n Opening key vector failed ");
-            return EXIT_FAILURE;
-        }
-        while ((line = fgets(buffer, sizeof(buffer), fstream)) != NULL) {
-            record = strtok(line, " ");
-            while (record != NULL) {
-                reverseKey(record);
-                printf("Recording: %s to [%d] \n", record, i);
-                strcpy(keys[i], record);
-                keys[i][14] = '\0';
-                record = strtok(NULL, " ");
-                i++;
-            }
-        }
-        return EXIT_SUCCESS;
+
+    int getSignature() {
+        return 0;
     }
 
-    char key[] = "12135267736472";
-    reverseKey(key);
-    //printf("%s", key);
-    loadKeys();
+    //adds the 2 keys together
+    char* addKey(char *key1, char *key2) {
+        int longest = 1 + (strlen(key1) + strlen(key2))/2.0 + abs(strlen(key1) - strlen(key2))/2.0;
+        char new1[longest];
+        char new2[longest]; 
+        memset(new1, '0', longest);
+        memset(new2, '0', longest);
+        strncpy(new1, key1, strlen(key1));
+        strncpy(new2, key2, strlen(key2));    
+        new1[longest] = '\0';
+        new2[longest] = '\0';
+
+        char placeValues[longest+1];
+        memset(placeValues, '\0', sizeof(placeValues));
+        int valueToAdd;
+        int carryOver = 0;
+        for (int i = 0; i<(longest-1); i++) {
+            //while we arent at the end of the string
+            while (i != (longest-2)) {
+                valueToAdd = (new1[i] - '0') + (new2[i] - '0') + carryOver;
+                carryOver = 0;
+                if (valueToAdd > 9 ) {
+                    valueToAdd = valueToAdd - 10;
+                    carryOver++;
+                }
+                placeValues[i] = valueToAdd + '0';
+                printf("%d placed at %d\n",valueToAdd,i);
+                i++;    
+            }
+            valueToAdd = (new1[i] - '0') + (new2[i] - '0') + carryOver;
+            carryOver = 0;
+            //the case when we reach the end of the string, the final digit to add
+            if (valueToAdd > 9) {
+                valueToAdd = valueToAdd - 10;
+                placeValues[(longest-2)] = valueToAdd + '0';
+                printf("%d into 2nd last element\n",valueToAdd);
+                carryOver++;
+                placeValues[(longest-1)] = carryOver + '0';
+                printf("%d into last element\n",carryOver);
+            }
+            else {
+                placeValues[(longest-2)] = valueToAdd + '0';
+                printf("%d placed at %d\n",valueToAdd,i);
+            }
+        }
+        reverseKey(placeValues);        
+        printf("%s",placeValues);
+    }
+       
+    addKey("12", "91");
+
+
+
 
   return 0;
 }
