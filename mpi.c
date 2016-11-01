6d106619ffaabc03819a3386676a26551974bb0e
wrsx <mewithmyclub@hotmail.com>
2016-11-02 03:19:29 +0800

    Send/Recv tests

--- mpi.c
+++ mpi.c
@@ -19,11 +19,30 @@ int main(int argc, char** argv) {
     MPI_Get_processor_name(processor_name, &name_len);
 
     // Print off a hello world message
+    /*
     printf("Hello world from processor %s, rank %d"
            " out of %d processors\n",
            processor_name, world_rank, world_size);
+    */
+  
+    // We are assuming at least 2 processes for this task
+    if (world_size < 2) {
+        fprintf(stderr, "World size must be greater than 1 for %s\n", argv[0]);
+        MPI_Abort(MPI_COMM_WORLD, 1);
+    }
+
+    int number;
+    if (world_rank == 0) {
+    // If we are rank 0, set the number to -1 and send it to process 1
+        number = -1;
+        MPI_Send(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+    } else if (world_rank == 1) {
+        MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("Process 1 received number %d from process 0\n", number);
+    }
 
     // Finalize the MPI environment.
     MPI_Finalize();
 }
+
 
